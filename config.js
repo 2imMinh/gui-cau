27421719aba47b754a6c94ffdc4e6f3c710f4053
2imMinh <86712112+2imMinh@users.noreply.github.com>
2021-10-16 18:43:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hi cậu!',
-    introDesc: `Có phải CNNer K53 đấy ko ?`,
-    btnIntro: 'Đúng roiii',
-    title: 'Mình cũng thế nè 😙',
-    desc: 'Inbox làm quen tí nhỉ ... ',
-    btnYes: 'Ok lunn',
-    btnNo: 'Không, còn lâu',
-    mess: 'Hehe thanks',
-    messDesc: 'Hẹn gặp ở trường nhóe!!!',
+    introTitle: 'Hi chào cậuuuuu!',
+    introDesc: `Mình có thể hỏi cậu 1 câu được khummmmm ?????`,
+    btnIntro: 'Hihi được lunnn',
+    title: 'Ai là ngừi xinh đẹp nhứt thế giới nhỉ ??????',
+    desc: 'Nếu cậu ko trả lời mà thoát ra tức là muốn làm người xấu xí đúng khum??????!!!!! ',
+    btnYes: 'Chính là những người phụ nữ E53 CNN <3',
+    btnNo: 'Chỉ có thể là người mẫu :v',
+    question:'Tại sao cậu lại trả lời đúng xuất sắc như vậy được ??? <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Đúng là người phụ nữ xinh nhất trong lòng mình 😘😘',
+    messDesc: 'Hãy nhận 1 phần quà nho nhỏ nhéeeeeee',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100011194722444'
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
