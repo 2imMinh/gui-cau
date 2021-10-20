30c671c930cd8a9a6e2cd8f585234ff9182d6ef4
2imMinh <86712112+2imMinh@users.noreply.github.com>
2021-10-20 09:58:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Đúng là người phụ nữ xinh nhất trong lòng mình 😘😘',
     messDesc: 'Hãy nhận 1 phần quà nho nhỏ nhân ngày 20/10 nhéeeeeee',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'file:///C:/Users/Admin/Documents/Zalo%20Received%20Files/From%20the%20boys%20with%203.pdf'
+    messLink: 'https://m.facebook.com/story.php?story_fbid=1470995126616964&id=100011194722444'
 }
